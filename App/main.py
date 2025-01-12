e929ea85c308e3bcdea147dcb5c99fd864519056
Rithik Raj <108055323+Rithikraj7@users.noreply.github.com>
2025-01-12 09:15:00 +0530

    Update main.py

--- App/main.py
+++ App/main.py
@@ -1,10 +1,26 @@
 from fastapi import FastAPI
 from App.routes import api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="PDF Question Answering System",
     description="Upload PDFs and ask questions about their content",
     version="1.0.0",
+)
+
+# Define allowed origins (front-end URL)
+origins = [
+    "http://192.168.50.1:3000",  # Your frontend URL during development
+    "http://192.168.50.1:3000/",      # Localhost for testing
+]
+
+# Add CORSMiddleware to the app
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows CORS for the specified origins
+    allow_credentials=True,
+    allow_methods=["*"],    # Allows all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],    # Allows all headers
 )
 
 app.include_router(api_router)
